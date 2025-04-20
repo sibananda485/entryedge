17b7f18d48dd9892c60b6d2e3f26a0091d21840b
sibananda485 <sahusiba485@gmail.com>
2025-04-20 19:14:37 +0530

    changes:AWS Credential moved to env file

--- server/src/config/s3.ts
+++ server/src/config/s3.ts
@@ -3,9 +3,8 @@ import { S3Client } from "@aws-sdk/client-s3";
 const s3Client = new S3Client({
   region: "ap-south-1",
   credentials: {
-    accessKeyId: "AKIASQ4R2MPAIGCTBUQK",
-    secretAccessKey: "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",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
   },
-
 });
 export default s3Client;
